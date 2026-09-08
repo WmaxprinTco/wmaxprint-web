f6135d7fe7a66037cdc54da8cbfede2ad8ef7c53
WmaxprinTco <laura.paezr@cun.edu.co>
2026-09-08 17:45:44 -0500

    actualiza script

--- Js/Script.js
+++ Js/Script.js
@@ -170,3 +170,80 @@ if (menus.length > 0) {
     });
 
 }
+
+/*---------- MENU HAMBURGUESA CELULAR ----------*/
+
+const menus = document.querySelectorAll(".menu");
+
+if (menus.length > 0) {
+
+    /* Crear botón */
+
+    const botonMenu = document.createElement("button");
+
+    botonMenu.classList.add("boton-menu-movil");
+
+    botonMenu.innerHTML = `
+        <span class="icono-hamburguesa">☰</span>
+        <span>Conoce nuestros servicios</span>
+    `;
+
+
+    /* Poner botón dentro del HEADER */
+
+    const header = document.querySelector(".top-header");
+
+    if (header) {
+
+        header.appendChild(botonMenu);
+
+    }
+
+
+    /* Abrir y cerrar menú */
+
+    botonMenu.addEventListener("click", () => {
+
+        document.body.classList.toggle("menus-movil-abiertos");
+
+
+        if (document.body.classList.contains("menus-movil-abiertos")) {
+
+            botonMenu.querySelector(".icono-hamburguesa").textContent = "✕";
+
+            botonMenu.querySelector("span:last-child").textContent =
+                "Cerrar servicios";
+
+        } else {
+
+            botonMenu.querySelector(".icono-hamburguesa").textContent = "☰";
+
+            botonMenu.querySelector("span:last-child").textContent =
+                "Conoce nuestros servicios";
+
+        }
+
+    });
+
+
+    /* Abrir categorías */
+
+    document.querySelectorAll(".menu .dropdown > a").forEach(enlace => {
+
+        enlace.addEventListener("click", function(e) {
+
+            if (window.innerWidth <= 768) {
+
+                e.preventDefault();
+
+                const padre = this.parentElement;
+
+                padre.classList.toggle("submenu-abierto");
+
+            }
+
+        });
+
+    });
+
+}
